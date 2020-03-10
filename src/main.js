6dd7c911f4d062189cc40710ca3af9aa30baf76e
Takamasa Arakawa <takamasa_arakawa@metaps.com>
2020-03-11 00:04:26 +0900

    add i18n and sample messages

--- src/main.js
+++ src/main.js
@@ -1,12 +1,33 @@
 import Vue from 'vue'
+import VueI18n from 'vue-i18n'
 import App from './App.vue'
 import router from './router'
-import vuetify from './plugins/vuetify';
+import vuetify from './plugins/vuetify'
 
+Vue.use(VueI18n)
 Vue.config.productionTip = false
+
+const messages = {
+  en: {
+    message: {
+      welcome: 'Welcome to Your Vue.js App'
+    }
+  },
+  ja: {
+    message: {
+      welcome: 'Vue.jsアプリケーションへようこそ'
+    }
+  }
+}
+
+const i18n = new VueI18n({
+  locale: 'ja',
+  messages
+})
 
 new Vue({
   router,
   vuetify,
+  i18n,
   render: h => h(App)
 }).$mount('#app')
