c3bacd6ef593e1b2357931f63c254cd0debbb1e3
Takamasa Arakawa <takamasa_arakawa@metaps.com>
2020-04-05 02:04:15 +0900

    remove old files

--- old/tests/unit/example.spec.js
+++
@@ -1,9 +0,0 @@
-import { shallowMount } from '@vue/test-utils';
-import New from '@/views/New.vue';
-describe('New.vue', function () {
-    it('renders props.msg when passed', function () {
-        var _wrapper = shallowMount(New, {});
-        // expect(wrapper.text()).toMatch(msg)
-    });
-});
-//# sourceMappingURL=example.spec.js.map
